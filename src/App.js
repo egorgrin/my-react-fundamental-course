1160c30b1ba664e90a06e3acf8d5ea428fec5420
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2023-12-08 19:14:52 +0500

    form-adding mechanism init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import MyInput from './components/UI/input/MyInput';
+import {useState} from 'react';
+import MyButton from './components/UI/button/MyButton';
+import PostList from './components/PostList';
 
 function App() {
+
+  const [posts, setPosts] = useState([{id: 1, title: 'JS 1', body: 'Decr'},]);
+
+  const [post, setPost] = useState(
+      {
+        title: '',
+        body: '',
+      },
+  );
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    setPosts([...posts, {...post, id: Date.now()}]);
+    setPost({title: '', body: ''});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={'App'}>
+        <form>
+          <MyInput
+              value={post.title}
+              onChange={e => setPost({...post, title: e.target.value})}
+              type={'text'}
+              placeholder={'Post Name'}
+          />
+          <MyInput
+              value={post.body}
+              onChange={e => setPost({...post, body: e.target.value})}
+              type={'text'}
+              placeholder="Content"
+          />
+
+          <MyButton onClick={addNewPost}>Create Post</MyButton>
+        </form>
+
+        <PostList posts={posts} title={'Js posts'}/>
+
+      </div>
   );
 }
 
